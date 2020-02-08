15f0650ae7b7173356f984e26161f8c8ab0cf2cf
EvgeniaKosyakABM <49563997+EvgeniaKosyakABM@users.noreply.github.com>
2020-02-08 20:59:07 +0200

    Update Task5.java

--- src/main/java/lesson1/Task5.java
+++ src/main/java/lesson1/Task5.java
@@ -1,15 +1,18 @@
 package lesson1;
 
-/**
- * Реализовать функцию, которая принимает строку и возвращает ее же в обратном виде
- * например "Hello world!!!" -> "!!!dlrow olleH"
- */
-public class Task5 {
+public class task5 {
+    /* Реализовать функцию, которая принимает строку и возвращает ее же в обратном виде
+     например "Hello world!!!" -> "!!!dlrow olleH" */
     public static void main(String[] args) {
-        //System.out.println(reverse("Hello world!!!"));
+        System.out.println(resFunc("Hello world!!!"));
     }
 
-    public static String reverse(String str) {
-        return "";
+    public static String resFunc(String phrase) {
+        String resStr = "";
+        char[] resMass = phrase.toCharArray();
+        for (int i = resMass.length-1; i>=0; i--) {
+             resStr= resStr + resMass[i];
+        }
+        return resStr;
     }
 }
