b426674a611555fca723ce6fafa7a3cf37f1eb62
EvgeniaKosyakABM <49563997+EvgeniaKosyakABM@users.noreply.github.com>
2020-02-08 20:58:26 +0200

    Update Task4.java

--- src/main/java/lesson1/Task4.java
+++ src/main/java/lesson1/Task4.java
@@ -1,29 +1,21 @@
 package lesson1;
 
-/* Мое имя
-Вывести на экран свое имя 5 строк по 10 раз (через пробел).
-System.out.println, System.out.print можно использовать только по одному разу
-для этого нужно воспользоваться циклом do () while
-*/
-
-public class Task4 {
-
+public class task4 {
+    /* Мое имя
+ Вывести на экран свое имя 5 строк по 10 раз (через пробел).
+ System.out.println, System.out.print можно использовать только по одному разу
+ для этого нужно воспользоваться циклом do () while */
     public static void main(String[] args) {
-        String s = "Vladimir";
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
-        System.out.println(
-                s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " " + s + " "
-                        + s);
+        String name = "Evgenia";
+        int i = 0;
+        do {
+            int j = 0;
+            do {
+                j++;
+                System.out.print(name + " ");
+            } while (j < 10);
+            i++;
+            System.out.println();
+        } while (i < 5);
     }
 }
