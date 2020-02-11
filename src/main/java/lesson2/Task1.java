82c247c917fef5cb426c687efaec686858c51924
Evgenia <evgenia.kosyak@gmail.com>
2020-02-11 12:11:49 +0200

    Commit1102

--- src/main/java/lesson2/Task1.java
+++ src/main/java/lesson2/Task1.java
@@ -21,4 +21,72 @@ package lesson2;
 
 public class Task1 {
 
+    final static String groupChild = "child";
+    final static String groupStudent = "student";
+    final static String groupWorker = "worker";
+    final static String groupPensioner = "pensioner";
+
+    public Task1(int age, String name){
+        this.age = age;
+        this.name = name;
+        setAgeGroup(age);
+    }
+
+    public Task1(){
+        this.age = 4;
+        this.name = "Esenia";
+        this.ageGroup = groupChild;
+    }
+
+    private int age;
+    private String name;
+    String ageGroup;
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        if(age < 0 || age > 100) {
+            System.out.print("Error age");
+        } else {
+            this.age = age;
+            setAgeGroup(age);
+        }
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        if( name == null ) {
+            System.out.println("Error name");
+            return;
+        }
+        if(name.length() < 3 || name.length() > 50 || name.trim().length() == 0 ) {
+            System.out.println("Error name");
+        } else {
+            this.name = name.substring( 0, 1 ).toUpperCase() + name.substring( 1 );
+        }
+    }
+
+    public String getAgeGroup() {
+        return this.ageGroup;
+    }
+
+    private void setAgeGroup(int age) {
+        if(age >= 0 || age <= 14) {
+            this.ageGroup = groupChild;
+        } else if (age >= 15 || age <= 25){
+            this.ageGroup = groupStudent;
+        } else if (age >= 26 || age <= 65){
+            this.ageGroup = groupWorker;
+        } else if (age >= 66 || age <= 100){
+            this.ageGroup = groupPensioner;
+        } else {
+            System.out.println("Error ageGroup");
+        }
+    }
+
 }
